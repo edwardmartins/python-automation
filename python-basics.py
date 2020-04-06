24218e7b6a05a84896316ec01c7ff37670a9829b
unknown <edumar03@ucm.es>
2020-04-06 13:26:18 +0200

    python basics

---
+++ python-basics.py
@@ -0,0 +1,13 @@
+# Simple program that ask for your name and age
+print("Hello world")
+
+print("What is your name?")
+myName = input()
+print('It is good to meet you ' + myName)
+print('The length of your name is: ' + str(len(myName)))
+
+print('What is your age?')
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) +' in a year')
+
+# Flow control
