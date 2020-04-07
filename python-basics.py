0738fc058c972959f97b5565eb4eadaa36dab925
unknown <edumar03@ucm.es>
2020-04-07 18:16:10 +0200

    section 6 - lists

--- python-basics.py
+++ python-basics.py
@@ -1,4 +1,5 @@
 # 1) Introduction
+# ------------------------------------------------------------
 print("Hello world")
 
 print("What is your name?")
@@ -12,13 +13,15 @@ print('You will be ' + str(int(myAge) + 1) +' in a year')
 
 
 # 2) Flow control
+# ------------------------------------------------------------
+# if-else
 password = 'swordfish'
 if password == 'swordfish':
     print('Acces granted')
 else:
     print('Wrong password')
 
-
+# if-elif
 name = 'Bob'
 age = 3000
 if name == 'Alice':
@@ -30,13 +33,13 @@ elif age > 2000:
 elif age > 100:
     print('You are not Alice, grannie')
 
-
+# while
 spam = 0
 while spam < 5:
     print('Hello world')
     spam = spam + 1
 
-
+# break
 name = ''
 while True:
     print('Please type your name')
@@ -45,6 +48,7 @@ while True:
         break
 print('Thank you')
 
+# continue
 spam = 0
 while spam < 5:
     spam = spam + 1
@@ -52,29 +56,32 @@ while spam < 5:
         continue
     print('spam is ' + str(spam))
 
+# for
 for i in range(5):
-    print('Jimmy Five times ' + str(i))
+    print(str(i))
 
 total = 0
 for num in range(101):
     total += num
 print(total)
 
+# for with increment by
 for i in range(0,10,2):
-    print('Jimmy Five times ' + str(i))
-
+    print(str(i))
 
 
 # 3) Functions and imports
-import pyperclip
-import random, sys, os, math
-from random import randint
-from math import *
+# ------------------------------------------------------------
+import pyperclip # single import
+import random, sys, os, math # multiple imports
+from random import randint # single function import
+from math import * # multiple function import
 
 print(random.randint(1,10))
 pyperclip.copy('Copied')
 print(pyperclip.paste())
 
+# functions
 def hello():
     print('Edward!!')
     print('Hello there!!')
@@ -94,8 +101,8 @@ newNumber = plusOne(5)
 print(newNumber)
 
 
-
 # 4) Try and except statements
+# ------------------------------------------------------------
 def divBy(number,divideBY):
     try:
         return number / divideBY
@@ -118,8 +125,8 @@ except ValueError:
     print('You did not enter a number')
 
 
-    
 # 5) Guess the number program
+# ------------------------------------------------------------
 import random
 
 print('Hello what is your name?')
@@ -144,4 +151,80 @@ else:
     print('You fail, the number I was thinking of was ' + str(secretNumber))
 
 print('You tried ' + str(guessTaken) + ' time/s')
-    
+
+
+# 6) Lists
+# ------------------------------------------------------------
+spam = ['car', 'bat', 'rat', 'elephant']
+print(spam)
+print('----------------------------------')
+print(spam[0]) # first item
+print(spam[-1]) # last item
+print(spam[1:3]) # slice between second and thrid item
+print(spam[:2]) # slice between first and second item
+print(spam[1:]) # slice between second and last item
+print(spam[:-1]) # slice between first and second last item
+print(spam[::-1]) # inverse the list
+print('----------------------------------')
+print('Making a list out of a string')
+characters = list('hello')
+print(characters)
+print('h' in characters)
+print('h' not in characters)
+
+# for loops with lists (index and value)
+supplies = ['pens', 'staplers', 'throwers', 'blinders']
+for i in range(len(supplies)):
+    print(str(i) + ' : ' + supplies[i])
+
+# multiple assignment
+cat = ['fat', 'orange', 'loud']
+size, color, disposition = cat
+print(size + ',' + color + ',' + disposition)
+
+size, color, disposition = 'skinny', 'black', 'quiet'
+print(size + ',' + color + ',' + disposition)
+
+a = 'AAA'
+b = 'BBB'
+a, b = b, a
+print(a + ',' +  b)
+
+# list methods
+spam = ['bat', 'rat', 'elephant']
+print(spam.index('bat')) # index of the first time the value is found in the list
+spam.append('dog') # add a value to the end of the list
+print(spam)
+spam.insert(1,'chicken') # insert a value in a particular position in the list
+print(spam)
+spam.remove('bat') # removes an specific value in the list
+del spam[2]
+print(spam) 
+
+spam = [2, 5, 3.14, 1, -7]
+spam.sort() # sorts a list
+print(spam)
+spam.sort(reverse=True) # reverse order
+print(spam)
+
+spam = ['bat', 'Rat', 'elephant', 'ant']
+spam.sort() # doesn't use true alphabetical order
+print(spam)
+spam.sort(key=str.lower) # true alphabetical order
+print(spam)
+
+# There are mutuable and inmutable datatypes
+# Mutable datatypes stores references 
+# Lists are mutable and strings unmutable
+# To modidy a string you have to create a new string
+name = 'Zophie a cat'
+newName = name[0:7] + 'the' + name[8:12]
+print(newName)
+
+# Copies the whole list and creates a new reference
+import copy
+spam = ['a', 'b','c','d']
+spamCopy = copy.deepcopy(spam) 
+spamCopy[0] = 'z'
+print(spam)
+print(spamCopy)
