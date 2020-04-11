a98a3531819ca8f6c8fbb6d8668b6219e43883e4
unknown <edumar03@ucm.es>
2020-04-11 11:50:12 +0200

    regex

--- 10_regular_expressions.py
+++ 10_regular_expressions.py
@@ -1,23 +1,36 @@
 # Regular expressions
 # --------------------------------------------------------
-# Find a phone number in the text
 import re
 
-text = 'Call me 415-555-1011 tomorrow, or at 415-555-9999 for my office line'
+# Find a phone number in the text
+text = 'Call me 415-555-1011 tomorrow, or at 415-555-9999 for my office line' 
 
-# Looking for 3 digits and a dash
-phone_number_regex = re.compile(r'\d\d\d-\d\d\d-\d\d\d') 
+# 1) We create a regex object with compile
+phone_num_regex = re.compile(r'\d\d\d-\d\d\d-\d\d\d') # looking for 3 digits(\d) and a dash
 
-# Find one ocurrence in the text
-match_object = phone_number_regex.search(text) 
+# 2) Then we create a match object
+match_object = phone_num_regex.search(text) # find one ocurrence in the text
 print(match_object.group())
 
-# Find all ocurrences in the text
-match_object = phone_number_regex.findall(text) 
+match_object = phone_num_regex.findall(text) # find all ocurrences in the text
 print(match_object)
 
+# We use parenthesis to create groups
+phone_num_regex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d)')
+mo = phone_num_regex.search(text)
+print(mo.group(1))
+print(mo.group(2))
 
+# If we want to search for a parenthesis in the text we use \(and\)
+phone_num_regex = re.compile(r'\(\d\d\d\) \d\d\d-\d\d\d')
+mo = phone_num_regex.search('Call me (415) 555-1011 tomorrow')
+print(mo.group())
 
+# Pipe character
+bat_regex = re.compile(r'Bat(man|mobile|copter|bat)') # looking for prefix "Bat" and any sufix in the list
+mo = bat_regex.search('Batmobile lost a wheel')
+print(mo.group())
+print(mo.group(1)) # with group(1) we know what suffix we found
 
 
 
