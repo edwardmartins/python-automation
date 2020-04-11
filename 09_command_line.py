6484d9b6f674c9ec2e6007d53d81e191ad3b16e0
unknown <edumar03@ucm.es>
2020-04-11 20:20:23 +0200

    update

--- 09_command_line.py
+++ 09_command_line.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 # Indicates the path to the interpreter the computer should use
 # we put linux shebang for compatibility, windows don't need it
-
 import sys
 print('Hello world')
 print(sys.argv) # command line arguments can be read in the sys.argv list
--- 10_regular_expressions.py
+++ 10_regular_expressions.py
@@ -9,10 +9,13 @@ text = 'Call me 415-555-1011 tomorrow, or at 415-555-9999 for my office line'
 phone_num_regex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d') # looking for 3 digits(\d) and a dash
 
 # 2) Then we create a match object
-match_object = phone_num_regex.search(text) # find one ocurrence in the text
+
+ # find one ocurrence in the text (returns a match object)
+match_object = phone_num_regex.search(text)
 print(match_object.group())
 
-match_object = phone_num_regex.findall(text) # find all ocurrences in the text
+# find all ocurrences in the text (returns a list of strings)
+match_list = phone_num_regex.findall(text) 
 print(match_object)
 
 # We use parenthesis to create groups
@@ -91,13 +94,132 @@ digit_regex = re.compile(r'\d{3,5}?')
 mo = digit_regex.search('123456789')
 print(mo.group())
 
-# Regex Character Classes
+# Regex find all method
 # --------------------------------------------------------
 
+# without groups (returns a list of strings)
+phone_regex = re.compile(r'\d{3}-\d{3}-\d{4}')
+ml = phone_regex.findall('Cell: 415-555-9999 Work: 212-555-0000')
+print(ml)
 
+# with groups (returns a list of tuples of strings)
+phone_regex = re.compile(r'((\d\d\d)-(\d\d\d-\d\d\d\d))')
+ml = phone_regex.findall('Cell: 415-555-9999 Work: 212-555-0000')
+print(ml)
 
+# Character classes
+# --------------------------------------------------------
+# \d =>  is a digit
+# \D =>  is not a digit
+# \w =>  is a letter or a digit
+# \W =>  is not a letter and not a digit
+# \s =>  is a space or a tab
+# \S =>  is not a space and not a tab
 
+xmas_regex = re.compile(r'\d+\s\w+')
+ml = xmas_regex.findall('12 drummers, 11 pipers, 10 lords, 9 ladies, 8 maids, 7 \
+            swans, 6 geese, 5 rings, 4 birds, 3 hens, 2 doves, 1 partridge')
+print(ml)
 
+# Making your own character classes
+# --------------------------------------------------------
+# regex_obj = re.compile(r'[xyz]') => with brackets same as x|y|z
 
+text = 'Robocop eats baby food'
+# Only vowels
+vowel_rege = re.compile(r'[aeiouAEIOU]') # [a|e|i|o|u]
+ml = vowel_rege.findall(text)
+print(ml)
 
+# The opposite of a vowel
+consonant_rege = re.compile(r'[^aeiouAEIOU]') 
+ml = consonant_rege.findall(text)
+print(ml)
 
+# Only letters
+only_letters_rege = re.compile(r'[a-zA-z]') 
+ml = only_letters_rege.findall(text)
+print(ml)
+
+# The match has to occur at the beggining of the string^( )
+begins_with = re.compile(r'^Hello')
+mo = begins_with.search('Hello there')
+print(mo.group())
+mo = begins_with.search('He said Hello')
+print(mo == None)
+
+# The match has to occur at the end of the string $( )
+ends_with = re.compile(r'world!$')
+mo = ends_with.search('Hello world!')
+print(mo.group())
+mo = ends_with.search('world! Hello')
+print(mo == None)
+
+# The match has to be the entire string
+all_digits = re.compile(r'^\d+$')
+mo = all_digits.search('123456789')
+print(mo.group())
+
+# The dot (.) character match any character except the new line
+at_regex = re.compile(r'.at') 
+ml = at_regex.findall('The cat in the hat sat on the flat mat')
+print(ml)
+
+at_regex = re.compile(r'.{1,2}at') 
+ml = at_regex.findall('The cat in the hat sat on the flat mat')
+print(ml)
+
+# (.*) means anything => take whataver you find after First Name and Last Name
+# When there is groups findAll returns the groups not the entire match
+text = 'First Name: Al Last Name: Sweigart'
+name_regex = re.compile(r'First Name: (.*) Last Name: (.*)') 
+ml = name_regex.findall(text)
+print(ml)
+
+# (.*) uses greedy mode, so is gonna take the maximum string possible
+# (.*?) non greedy mode, the minimun string possible
+serve = '<To serve humans> for dinner>'
+nongreedy = re.compile(r'<(.*?)>')
+ml = nongreedy.findall(serve)
+print(ml)
+
+greedy = re.compile(r'<(.*)>')
+ml = greedy.findall(serve)
+print(ml)
+
+# Include new line characters in (.*) expression
+prime = 'Serve the public trust. \nProtect the innocent. \nUpload the law.'
+dot_star = re.compile(r'.*', re.DOTALL) 
+mo = dot_star.search(prime)
+print(mo.group())
+
+# Case sensitive matching
+vowel_regex_caps = re.compile(r'[aeiou]', re.IGNORECASE)
+ml = vowel_regex_caps.findall('Al, why does your programming book talk about Robocop so much?')
+print(ml)
+
+# Find and replace with the sub() method
+# --------------------------------------------------------
+text = 'Agent Alice gave the secret documents to Agent Bob'
+
+# Sub method not only finds, also replaces
+names_regex = re.compile(r'Agent \w+')
+res = names_regex.sub('REDACTED',text)
+print(res)
+
+# Sub methods replaces with groups results
+names_regex = re.compile(r'Agent (\w)\w*') # group 1 -> A or B
+res = names_regex.sub(r'Agent \1****',text) # \1 means use the text for group 1
+print(res)
+
+# Verbose format ( let you add white space and comments)
+# --------------------------------------------------------
+re.compile(r'''
+
+(\d\d\d-) |     # area code without parens
+(\(\d\d\d\))    # or area code with parens   
+\d\d\d          # first 3 digits
+-               # second dash
+\d\d\d\d        # last 4 digits
+
+''', re.VERBOSE | re.DOTALL | re.IGNORECASE) # you can combine multiple options in the second argument
